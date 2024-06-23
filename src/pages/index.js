53a4263f927d130517528f5ce6021a13337573b1
Todd <ihatespam@gmail.com>
2024-06-23 16:20:36 -0500

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Seo from "../components/seo";
+import Layout from "../components/siteLayout";
+import { Link } from "gatsby"
+import { RiArrowLeftSLine, RiCheckboxCircleLine } from "react-icons/ri"
+import { Helmet } from "react-helmet"
+
+const HomeTemp = () => {
+      return (
+
+ <Layout className="thanks-page">
+
+<Helmet>
+
+  
+<body id="body" className="install homepage " />
+</Helmet>
+
+ <Seo title={`Thanks for being a Pirate!`} />
+
+
+<section className="outer section section--gradient" >
+      <div className="container" style={{padding: '30px 0', minHeight:'100dvh'}}>
+        
+      {/* <div className="mobile"><GoBack /></div> */}
+
+
+    
+
+<div style={{width:'90%', height:'100px', margin:'0 auto', textAlign:'center',}}>
+
+{/* <p>
+<strong>Hytron Manufacturing, Inc.</strong><br />
+15582 Chemical Lane<br />
+Huntington Beach, CA 92649<br />
+</p> */}
+
+
+
+
+
+
+      <h1 className="" style={{fontSize:'50px',}}>Coming Soon!</h1>
+      <div className="spacer33"></div> 
+      {/* <Link to="/" className="button">
+        <RiArrowLeftSLine className="icon -left" />
+        Back to Homepage
+      </Link> */}
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+   
+
+
+    </section>
+    
+    
+    </Layout>
+  );
+};
+
+export default HomeTemp;
